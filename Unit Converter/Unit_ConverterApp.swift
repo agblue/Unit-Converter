9069c33747fc83c54731984a82e44f0528c0afad
Danny Tsang <danny.tsang@me.com>
2021-08-12 15:18:48 -0500

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Danny Tsang on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
